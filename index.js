dc6bf7fc947fe6aba2a2e9cc80a02fe404ee962f
makawizy <makaone@gmail.com>
2023-04-26 12:04:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,12 +25,9 @@ const CONNECTION_URL = process.env.LOCALHOST_CONNECTION_URL;
 const PORT = process.env.PORT || 5000;
 console.log(CONNECTION_URL);
 
-try{
-    mongoose.connect(CONNECTION_URL, {useNewUrlParser:true,useUnifiedTopology:true });
-}catch(error){
-    console.log(error);
-    process.exit(1);
-}
+mongoose.connect(CONNECTION_URL, {useNewUrlParser:true,useUnifiedTopology:true })
+.then(() => app.listen(PORT, () => console.log(`Server running port : ${PORT}`)))
+.catch((err) =>console.log(err.message));
 
 
 //mongoose.set(useFindAndModify, false);

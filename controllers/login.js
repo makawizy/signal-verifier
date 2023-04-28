31d26da2a356c3d1c209268b11fd82db37b91f31
makawizy <makaone@gmail.com>
2023-04-28 22:15:22 +0100

    Update login.js

--- controllers/login.js
+++ controllers/login.js
@@ -7,6 +7,7 @@ export const signIn = async (req, res, next) => {
     try {
        const user = await Login.findOne({email: req.body.email});
        const loginEmail = req.body.email;
+        console.log(loginEmail);
         if(!user) return next(createError(404, `CANNOT FIND USER : ${loginEmail}`));
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
         if(!isPasswordCorrect) return next(createError(400,"Wrong Login Incredentials!"))

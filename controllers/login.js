d22366ec662ebfb3cd16e1ad94fc8de60793688a
lawal <makaone@gmail.com>
2023-04-29 13:17:11 +0100

    modified:   controllers/login.js

--- controllers/login.js
+++ controllers/login.js
@@ -10,13 +10,9 @@ export const signIn = async (req, res, next) => {
         console.log(loginEmail);
         if(!user) return next(createError(404, `CANNOT FIND USER : ${loginEmail}`));
         const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
-        if(!isPasswordCorrect) return next(createError(400,"Wrong Login Incredentials!"))
-        const token = jwt.sign({id: user._id},process.env.JWT);
+        if(!isPasswordCorrect) return next(createError(400,"Wrong Login Incredentials!"));
         const {password, ...otherDetails} = user._doc;
-     res
-     .cookie("access_token", token, {httpOnly:true})
-     .status(200)
-     .json({...otherDetails});
+     res.status(200).json({...otherDetails});
     } catch (error) {
         next(createError(500, error.message));
     }

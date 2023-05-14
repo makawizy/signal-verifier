2ee9c87a0a845806a83db7ed12ff105a23583a87
lawal <makaone@gmail.com>
2023-05-14 20:36:36 +0100

    register route added

---
+++ controllers/register.js
@@ -0,0 +1,35 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import Login from '../models/login.js';
+import { createError } from '../util/error.js';
+
+export const register = async (req, res, next) => {
+    try {
+       const User = await Login.findOne({email: req.body.email});
+       const registerEmail = req.body.email;
+        console.log(registerEmail);
+        if(User) return next(createError(404, `Email already exist : ${registerEmail}`));
+        
+        try {
+            const saltRounds = 10;
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+            const newUser = new User({
+             email: req.body.email,
+             password: hashedPassword,
+             surname: req.body.surname ,
+             middlename: req.body.middlename,
+             othername: req.body.othername,
+         });
+         await newUser.save();
+         const {password, ...otherDetails} = req.body;
+         res.status(200).send({...otherDetails});
+        } catch (error) {
+            next(createError(500, error.message));
+        }
+        console.log("registration success");
+     res.status(200).json({...otherDetails});
+    } catch (error) {
+        next(createError(500, error.message));
+    }
+};

922cce3a7da0c65c3743ca50d79e6fbe03dfbd3d
lawal <makaone@gmail.com>
2023-05-21 23:31:02 +0100

    modified:   index.js 	modified:   models/login.js 	controllers/culprits.js 	models/culprits.js 	routes/culprits.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from 'cors';
 import login_router from './routes/login.js';
 import register_router from './routes/register.js';
 import profile_router from './routes/get_profile.js';
+import culprits_router from './routes/culprits.js'
 //import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
 
@@ -19,6 +20,7 @@ app.use(cors());
 app.use('/login',login_router);
 app.use('/register', register_router)
 app.use('/profile', profile_router)
+app.use('/culprits', culprits_router)
 
 app.get('/', (req, res) =>{
     res.send("Welcome To Signal Verifier API Homepage");

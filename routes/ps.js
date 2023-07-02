5cf77f388eec9d383360ce52beb2b9a59190081d
Lawal Musa Jiya <makaone@gmail.com>
2023-07-02 14:44:53 +0100

    take_ps

--- routes/ps.js
+++ routes/ps.js
@@ -1,11 +1,12 @@
 
 import express from 'express';
-import { create_ps, get_ps, update_ps } from '../controllers/ps.js';
+import { create_ps, get_ps, update_ps, take_ps } from '../controllers/ps.js';
 
 const ROUTER = express.Router();
 
 ROUTER.post('/create_ps', create_ps);
 ROUTER.post('/get_ps', get_ps);
 ROUTER.patch('/update_ps/:id', update_ps);
+ROUTER.patch('/take_ps/:id', update_ps);
 
 export default ROUTER;

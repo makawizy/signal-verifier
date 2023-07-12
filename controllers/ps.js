533cf1226ef4892cb28381ea5b1e293a4c022867
Lawal Musa Jiya <makaone@gmail.com>
2023-07-12 13:19:07 +0100

    records

--- controllers/ps.js
+++ controllers/ps.js
@@ -76,7 +76,7 @@ export const insert_loadRecords = async (req, res, next) => {
 
             const loadRecords = await PS.updateOne({ _id },
                 
-                { $addToSet: { 'records': { $each  : records } } },
+                { $addToSet: { 'ps.records': { $each: [records], $slice: -1 } } },
                 );
             res.status(200).json(loadRecords)
 

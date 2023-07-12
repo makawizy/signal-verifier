81f64b08ec0cdad1f97dfe6f6600d31a1da73e00
Lawal Musa Jiya <makaone@gmail.com>
2023-07-12 12:52:33 +0100

    ps

--- controllers/ps.js
+++ controllers/ps.js
@@ -75,7 +75,7 @@ export const insert_loadRecords = async (req, res, next) => {
         const record = req.body;
         const ps = await PS.findOne({ _id : _id});
         const records = ps.records.some(recordObj => recordObj.army_number === record.army_number) ;
-        console.log(records);
+        console.log(ps);
         if (records.length > 0) {
             const loadRecords = await PS.updateOne({ _id },
                 

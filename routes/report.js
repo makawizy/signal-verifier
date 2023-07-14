f8a14be1557b80a69a943e25849fc7b0c2ec8f73
Lawal Musa Jiya <makaone@gmail.com>
2023-07-14 12:04:09 +0100

    psId

--- routes/report.js
+++ routes/report.js
@@ -4,6 +4,6 @@ import { cancel_existing_report, create_report } from '../controllers/report.js'
 const ROUTER = express.Router();
 
 ROUTER.patch('/cancel/:id', cancel_existing_report);
-ROUTER.patch('/create/:id', create_report);
+ROUTER.post('/create/:id', create_report);
 
 export default ROUTER;

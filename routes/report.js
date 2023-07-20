b997715dad2b7c4ce119b24390acfa0b56ee761c
Lawal Musa Jiya <makaone@gmail.com>
2023-07-20 10:08:40 +0100

    patch

--- routes/report.js
+++ routes/report.js
@@ -5,7 +5,7 @@ const ROUTER = express.Router();
 
 ROUTER.patch('/cancel/:id', cancel_existing_report);
 ROUTER.post('/create/:id', create_report);
-ROUTER.post('/take_report/:id', take_report);
+ROUTER.patch('/take_report/:id', take_report);
 
 
 export default ROUTER;
